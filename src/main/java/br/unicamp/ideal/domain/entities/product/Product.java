4604e7ad11bf73417c9256cd71f858c6472365af
glayson0 <gn.btista@gmail.com>
2026-09-01 12:17:13 -0300

    refactor(Product): add unique id, change status initialization, change exceptions and do some YAGNI changes (#23)

--- src/main/java/br/unicamp/ideal/domain/entities/product/Product.java
+++ src/main/java/br/unicamp/ideal/domain/entities/product/Product.java
@@ -1,21 +1,23 @@
 package br.unicamp.ideal.domain.entities.product;
 
 public class Product {
-    private int id;
-    private String name;
-    private ProductStatus status;
-    private int rawMaterialAmountNeeded;
+    private static int globalUniqueId = 1;
 
-    public Product(int id, String name, ProductStatus status, int rawMaterialNeeded) {
-        this.id = id;
+    private final int id;
+    private final String name;
+    private ProductStatus status =  ProductStatus.UNPROCESSED;
+    private final int rawMaterialAmountNeeded;
+
+    public Product(String name, int rawMaterialNeeded) {
+        this.id = globalUniqueId++;
         this.name = name;
-        this.status = status;
         this.rawMaterialAmountNeeded = rawMaterialNeeded;
     }
 
     public void process() {
-        if (status == ProductStatus.PROCESSED)
-            throw new IllegalStateException("The product is already processed.");
+        if (status == ProductStatus.PROCESSED) {
+            throw new IllegalStateException("[DESSA VEZ NÃO É] The product is already processed.");
+        }
         setStatus(ProductStatus.PROCESSED);
     }
 
@@ -23,16 +25,8 @@ public class Product {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public ProductStatus getStatus() {
@@ -45,10 +39,6 @@ public class Product {
 
     public int getRawMaterialAmountNeeded() {
         return rawMaterialAmountNeeded;
-    }
-
-    public void setRawMaterialAmountNeeded(int rawMaterialAmountNeeded) {
-        this.rawMaterialAmountNeeded = rawMaterialAmountNeeded;
     }
 
     @Override
