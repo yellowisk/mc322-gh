9fe5e63ba6845de3a44c172ef2f504a1aa986f4b
yellowisk <h245293@dac.unicamp.br>
2026-09-21 22:11:18 -0300

    feat(ProductionStrategy): add ProductionStrategy interface

---
+++ src/main/java/domain/interfaces/ProductionStrategy.java
@@ -0,0 +1,17 @@
+package domain.interfaces;
+
+import domain.entities.demand.Demand;
+import java.util.List;
+
+public interface ProductionStrategy {
+    Demand selectDemand(List<Demand> demands, double availableBudget);
+
+    String getStrategyName();
+
+    /** Same filter for all three strategies */
+    default List<Demand> selectable(List<Demand> demands) {
+        return demands.stream()
+                .filter(d -> d.getStatus().isSelectable())
+                .toList();
+    }
+}
