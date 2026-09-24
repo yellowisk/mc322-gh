03c81e46b5d23f8fe6f491c12f75a30cada2e2d5
yellowisk <h245293@dac.unicamp.br>
2026-09-23 21:03:02 -0300

    feat(strategies): add `FilaDoCaixa` and `Ferrari` strategies

---
+++ src/main/java/domain/strategies/FilaDoCaixa.java
@@ -0,0 +1,22 @@
+package domain.strategies;
+
+import domain.entities.demand.Demand;
+import domain.interfaces.ProductionStrategy;
+
+import java.util.List;
+
+/* FIFOOOOOOOOOOO */
+public class FilaDoCaixa implements ProductionStrategy {
+
+    @Override
+    public Demand selectDemand(List<Demand> demands, double availableBudget) {
+        return selectable(demands).stream()
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public String getStrategyName() {
+        return "Fila do Caixa";
+    }
+}
