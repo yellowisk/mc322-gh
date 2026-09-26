27f8d69eba1a851eba51dc312ca27705cece91a3
yellowisk <h245293@dac.unicamp.br>
2026-09-26 14:46:10 -0300

    chore(exceptions): add serialVersionUID to custom exceptions

--- src/main/java/domain/exceptions/InsufficientBudgetException.java
+++ src/main/java/domain/exceptions/InsufficientBudgetException.java
@@ -1,6 +1,8 @@
 package domain.exceptions;
 
 public class InsufficientBudgetException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public InsufficientBudgetException(String message) {
         super(message);
     }
--- src/main/java/domain/exceptions/MachineNeedsRepairException.java
+++ src/main/java/domain/exceptions/MachineNeedsRepairException.java
@@ -1,6 +1,8 @@
 package domain.exceptions;
 
 public class MachineNeedsRepairException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public MachineNeedsRepairException(String message) {
         super(message);
     }
