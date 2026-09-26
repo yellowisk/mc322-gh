23c0012729e018575dc60022510ffadff017988a
yellowisk <h245293@dac.unicamp.br>
2026-09-26 14:46:10 -0300

    chore(exceptions): add serialVersionUID to custom exceptions

--- src/main/java/domain/exceptions/InvalidDemandTransitionException.java
+++ src/main/java/domain/exceptions/InvalidDemandTransitionException.java
@@ -1,6 +1,8 @@
 package domain.exceptions;
 
 public class InvalidDemandTransitionException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public InvalidDemandTransitionException(String message) {
         super(message);
     }
